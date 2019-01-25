d4826552ab0f65bb7a124b1561898bc65dbb4f13
Mateusz Michalowski <mateusz@adhese.eu>
2019-01-25 22:48:33 +0100

    Break the chatbot for demo

--- src/main/java/com/tdd/calculator/Calculator.java
+++ src/main/java/com/tdd/calculator/Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
         return a + b;
     }
 
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
     public int abs(int a) {
         if (a >= 0) {
             return a;
